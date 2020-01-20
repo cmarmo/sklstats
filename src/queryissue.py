858ef746f3c29f066ab5e04b5534390dfd0e3115
Chiara Marmo <chiara.marmo@inria.fr>
2020-01-20 12:57:14 +0100

    Add first comment to the issue list. Reformat and generalize query script.

--- src/queryissue.py
+++ src/queryissue.py
@@ -1,6 +1,7 @@
 # An example to get skl issues using the Github GraphQL API.
 # from https://gist.github.com/gbaman/b3137e18c739e0cf98539bf4ec4366ad
 
+import sys
 import requests
 import json
 
@@ -19,66 +20,81 @@ def run_query(query, variables):
         raise Exception("Query failed to run by returning code of {}.{}".
                          format(request.status_code, query))
 
-# The GraphQL query (with a few aditional bits included) itself
-# defined as a multi-line string.       
-firstquery = """
-query {
-  repository(owner: "scikit-learn", name: "scikit-learn") {
-    issues(first: 100) {
-      edges {
-        cursor
-        node {
-          number
-          createdAt
-          closedAt
-          author {
-            login
-          }
-          state
-          lastEditedAt
-        }
-      }
-    }
-  }
-}
-"""
-variables = {}
-result = run_query(firstquery, variables) # Execute the query
-templist = result["data"]["repository"]["issues"]["edges"]
-issuelist = templist
 
-myCursor = issuelist[-1]['cursor']
+## The main
+def main():
 
-while(len(templist)>1):
-        query = """
-        query($issueCursor: String!) {
-          repository(owner: "scikit-learn", name: "scikit-learn") {
-            issues(first: 100 after: $issueCursor) {
+  owner = sys.argv[1]
+  reponame = sys.argv[2]
+
+
+  # The GraphQL query (with a few aditional bits included) itself
+  # defined as a multi-line string.
+
+  firstquery = """
+  query {
+    repository(owner: \"""" + owner + """\", name: \"""" + reponame + """\" ) {
+      issues(first: 100) {
+  """
+
+  loopquery = """
+  query($issueCursor: String!) {
+    repository(owner: \"""" + owner + """\", name: \"""" + reponame + """\" ) {
+      issues(first: 100 after: $issueCursor) {
+  """
+
+  query = """
+        edges {
+          cursor
+          node {
+            number
+            createdAt
+            closedAt
+            author {
+              login
+            }
+            state
+            lastEditedAt
+            comments(first: 1) {
               edges {
-                cursor
                 node {
-                  number
-                  createdAt
-                  closedAt
                   author {
                     login
                   }
-                  state
-                  lastEditedAt
+                  createdAt
                 }
               }
             }
           }
         }
-        """
+      }
+    }
+  }
+  """
 
-        variables = {"issueCursor": myCursor}	
+  firstquery += query 
+  variables = {}
 
-        result = run_query(query, variables) # Execute the query
-        templist = result["data"]["repository"]["issues"]["edges"]
-        issuelist += templist
-        myCursor = issuelist[-1]['cursor']
+  result = run_query(firstquery, variables) # Execute the query
+  templist = result["data"]["repository"]["issues"]["edges"]
+  issuelist = templist
 
-fp = open('issues.json', 'w')
-json.dump(issuelist, fp)
-fp.close()
+  myCursor = issuelist[-1]['cursor']
+
+  loopquery += query
+
+  while(len(templist)>1):
+
+          variables = {"issueCursor": myCursor}	
+
+          result = run_query(loopquery, variables) # Execute the query
+          templist = result["data"]["repository"]["issues"]["edges"]
+          issuelist += templist
+          myCursor = issuelist[-1]['cursor']
+
+  fp = open('issues.json', 'w')
+  json.dump(issuelist, fp)
+  fp.close()
+
+if __name__ == "__main__":
+    main()
